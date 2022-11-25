8c9b0579511288d1a11700fe53d43aef1cc90b3c
patrizineversuxxx <r2tr@yandex.ru>
2022-11-25 16:08:43 +0400

    Minor code changes

--- ApartmentInfo.py
+++ ApartmentInfo.py
@@ -3,10 +3,10 @@ import RenovationType
 class ApartmentInfo:
     def __init__(self, 
             square : float, 
-            rooms_count : int, 
+            room_number : int, 
             smartins_count : int, 
-            apart_height : float, 
-            apart_floor : int, 
+            height : float, 
+            floor : int, 
             has_balcony : bool, 
             is_furnitured : bool, 
             renovation_type : RenovationType, 
@@ -14,10 +14,10 @@ class ApartmentInfo:
             household_features : str
     ):
         self._square = square
-        self._rooms_count = rooms_count
+        self._room_number = room_number
         self._smartins_count = smartins_count
-        self._apart_height = apart_height
-        self._apart_floor = apart_floor
+        self._height = height
+        self._floor = floor
         self._has_balcony = has_balcony
         self._is_furnitured = is_furnitured
         self._renovation_type = renovation_type
@@ -29,20 +29,20 @@ class ApartmentInfo:
         return self._square
 
     @property
-    def rooms_count(self) -> int:
-        return self._rooms_count
+    def room_number(self) -> int:
+        return self._room_number
 
     @property
     def smartins_count(self) -> int:
         return self._smartins_count
 
     @property
-    def apart_height(self) -> float:
-        return self._apart_height
+    def height(self) -> float:
+        return self._height
 
     @property
-    def apart_floor(self) -> int:
-        return self._apart_floor
+    def floor(self) -> int:
+        return self._floor
 
     @property
     def has_balcony(self) -> bool:
