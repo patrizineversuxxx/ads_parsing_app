4ed3a41ba1f597b8c6be139ee50309fe0721ae74
patrizineversuxxx <r2tr@yandex.ru>
2022-12-15 15:14:35 +0400

    Created apartment info converter

--- model/data_converters.py
+++ model/data_converters.py
@@ -9,15 +9,47 @@ from dto.rules import *
 from dao.dbclass import db
 
 
+class ApartmentInfoConverter:
+    @staticmethod
+    def to_dao(apartment_info: ApartmentInfo) -> ApartmentInfoDAO:
 
+        return ApartmentInfoDAO(address=apartment_info.address,
+                                square=apartment_info.square,
+                                room_number=apartment_info.room_number,
+                                smartin_number=apartment_info.smartin_number,
+                                height=apartment_info.height,
+                                floor=apartment_info.floor,
+                                has_balcony=apartment_info.has_balcony,
+                                is_furnitured=apartment_info.is_furnitured,
+                                renovation_type=apartment_info.renovation_type.value,
+                                features=apartment_info.features,
+                                household_features=apartment_info.household_features
+
+                                )
+
+    @staticmethod
+    def to_dto(apartment_info_dao: ApartmentInfoDAO) -> ApartmentInfo:
+
+        return ApartmentInfo(apartment_info_dao.address,
+                             apartment_info_dao.square,
+                             apartment_info_dao.room_number,
+                             apartment_info_dao.smartin_number,
+                             apartment_info_dao.height,
+                             apartment_info_dao.floor,
+                             apartment_info_dao.has_balcony,
+                             apartment_info_dao.is_furnitured,
+                             RenovationType(
+                                 apartment_info_dao.renovation_type),
+                             apartment_info_dao.features,
+                             apartment_info_dao.household_features
+                             )
 
 
 class BuildingInfoConverter:
     @staticmethod
     def to_dao(building_info: BuildingInfo) -> BuildingInfoDAO:
-        building_info = BuildingInfo(BuildingType.Brick, True, False, 4)
 
-        return BuildingInfoDAO(building_type=building_info.building_type,
+        return BuildingInfoDAO(building_type=building_info.building_type.value,
                                is_new_building=building_info.is_new_building,
                                has_elevator=building_info.has_elevator,
                                floor_number=building_info.floor_number
@@ -25,23 +57,21 @@ class BuildingInfoConverter:
 
     @staticmethod
     def to_dto(building_info_dao: BuildingInfoDAO) -> BuildingInfo:
-        building_info_dao = BuildingInfoDAO(building_type=3,
-                                            is_new_building=True,
-                                            has_elevator=True,
-                                            floor_number=113)
-        return BuildingInfo(building_info_dao.building_type,
+
+        return BuildingInfo(BuildingType(building_info_dao.building_type),
                             building_info_dao.is_new_building,
                             building_info_dao.has_elevator,
                             building_info_dao.floor_number
                             )
+
 
 class RulesConverter:
     @staticmethod
     def to_dao(rules: Rules) -> RulesDAO:
 
         return RulesDAO(apart_capacity=rules.apart_capacity,
-                        is_kids_allowed=rules.is_kids_allowed,
-                        is_animals_allowed=rules.is_animals_allowed,
+                        is_kids_allowed=rules.is_kids_allowed.value,
+                        is_animals_allowed=rules.is_animals_allowed.value,
                         utility_payments=rules.utility_payments,
                         has_prepayment=rules.has_prepayment)
 
@@ -50,7 +80,7 @@ class RulesConverter:
 
         return Rules(rules_dao.apart_capacity,
                      rules_dao.is_kids_allowed,
-                     rules_dao.is_animals_allowed,
-                     rules_dao.utility_payments,
+                     Allowance(rules_dao.is_animals_allowed),
+                     Allowance(rules_dao.utility_payments),
                      rules_dao.has_prepayment
                      )
